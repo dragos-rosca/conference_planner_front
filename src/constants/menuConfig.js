e49c03639f128e612b291bd37a7e00fd0be1b4e3
Dragos Rosca <drosca@totalsoft.ro>
2021-09-21 15:42:22 +0300

    initial commit

---
+++ src/constants/menuConfig.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dashboard from '@material-ui/icons/Dashboard'
+import Settings from '@material-ui/icons/Settings'
+
+const menuItems = [
+  { icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },
+  { icon: <Settings />, text: 'NavBar.Settings', path: '/settings', name: 'Settings' }
+]
+
+export default menuItems
