8b97415c333a1f2855c1139cbdbbbf77abb703a4
Dragos Rosca <drosca@totalsoft.ro>
2021-09-22 17:30:12 +0300

    landing page - step 5 - task 3 - completed

--- src/features/welcome/Welcome.js
+++ src/features/welcome/Welcome.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { Typography, Grid, InputAdornment } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 import CustomTextField from '@bit/totalsoft_oss.react-mui.custom-text-field'
 import IconButton from '@bit/totalsoft_oss.react-mui.icon-button'
 import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn'
+import { useEmail } from 'hooks/useEmail'
+import { validateEmail } from 'utils/functions'
+import { emptyString } from 'utils/constants'
 
 function Welcome() {
   const { t } = useTranslation()
+  const [email, setEmail] = useEmail()
+  const [isInputValid, setIsInputValid] = useState(true)
+  const [inputValue, setInputValue] = useState(email)
+
+  const handleInputChange = useCallback(e => setInputValue(e?.target?.value), [])
+  const handleButtonClick = useCallback(() => {
+    const isValid = validateEmail(inputValue)
+    setEmail(isValid ? inputValue : emptyString)
+    setIsInputValid(isValid)
+  }, [inputValue, setEmail])
+  const handleKeyDown = useCallback(e => e.keyCode === 13 && handleButtonClick(), [handleButtonClick])
 
   return (
     <Grid container direction='column' alignItems='center' spacing={10}>
@@ -19,13 +33,18 @@ function Welcome() {
         </Grid>
         <Grid item>
           <CustomTextField
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
             endAdornment={
               <InputAdornment position='end'>
-                <IconButton size='small' color='theme' aria-label='go'>
+                <IconButton size='small' color='theme' aria-label='go' onClick={handleButtonClick}>
                   <KeyboardReturnIcon fontSize='small' />
                 </IconButton>
               </InputAdornment>
             }
+            error={!isInputValid}
+            helperText={!isInputValid && t('LandingPage.BadEmail')}
           />
         </Grid>
       </Grid>
