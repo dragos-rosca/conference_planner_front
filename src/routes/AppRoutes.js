babc23043b0d61259a928523e8e740d5a86572c7
Dragos Rosca <drosca@totalsoft.ro>
2021-09-23 18:01:44 +0300

    attendee list

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -9,6 +9,7 @@ import Settings from 'features/settings/Settings'
 import { Forbidden, NotFound } from '@bit/totalsoft_oss.react-mui.kit.core'
 import HelloWorld from 'features/helloWorld/HelloWorld'
 import { useEmail } from 'hooks/useEmail'
+import ConferenceListContainer from 'features/conferences/components/ConferenceListContainer'
 
 export default function AppRoutes() {
   const [email] = useEmail()
@@ -26,10 +27,12 @@ export default function AppRoutes() {
     <Switch>
       <CustomRoute isPrivate={false} exact path='/helloWorld' component={HelloWorld} />
       <CustomRoute isPrivate={false} exact path='/welcome' component={Welcome} />
+      <CustomRoute isPrivate={false} exact path='/conferences' component={ConferenceListContainer} />
       <CustomRoute exact path='/settings' component={Settings} />
       <Redirect exact from='/' to='/welcome' />
       <CustomRoute isPrivate={false} exact path='/forbidden' component={Forbidden} />
       <CustomRoute isPrivate={false} render={() => <NotFound title='PageNotFound'></NotFound>} />
+
     </Switch>
   )
 }
