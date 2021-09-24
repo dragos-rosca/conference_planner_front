44df0eda0263054923c8699d850ecc0b4b8adf4e
Dragos Rosca <drosca@totalsoft.ro>
2021-09-24 16:29:59 +0300

    finished info and location display

---
+++ src/features/myConference/edit/components/MyConferenceContainer.js
@@ -0,0 +1,43 @@
+import LoadingFakeText from '@bit/totalsoft_oss.react-mui.fake-text/dist/LoadingFakeText'
+import SaveButton from '@bit/totalsoft_oss.react-mui.save-button'
+import MyConferenceHeader from 'features/myConference/list/components/MyConferenceHeader'
+import { useHeader } from 'providers/AreasProvider'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { categories, cities, counties, countries, types } from 'utils/mocks/conferenceDictionaries'
+import MyConference from './MyConference'
+
+const MyConferenceContainer = () => {
+  const { t } = useTranslation()
+  const [, setHeader] = useHeader()
+
+  useEffect(() => () => setHeader(null), []) // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    setHeader(<MyConferenceHeader actions={<SaveButton title={t('General.Buttons.Save')} />} />)
+  }, [setHeader, t])
+
+  const { data, loading } = {
+    loading: false,
+    data: {
+      typeList: types,
+      categoryList: categories,
+      countryList: countries,
+      countyList: counties,
+      cityList: cities
+    }
+  }
+
+  if (loading) return <LoadingFakeText lines={10} />
+
+  return (
+    <MyConference
+      types={data?.typeList}
+      categories={data?.categoryList}
+      countries={data?.countryList}
+      counties={data?.countyList}
+      cities={data?.cityList}
+    />
+  )
+}
+
+export default MyConferenceContainer
---
+++ src/features/myConference/edit/components/MyConferenceSpeakers.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const MyConferenceSpeakers = () => <>Hello World!</>
+
+export default MyConferenceSpeakers
